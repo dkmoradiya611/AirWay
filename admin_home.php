be06d019759021599e33f2eaf2a8fb5763debe82
dharmika-shakti <dharmika.shaktiwebsolutions@gmail.com>
2023-09-14 15:53:30 +0530

    Some Editing

--- admin_home.php
+++ admin_home.php
@@ -1,3 +1,27 @@
+<?php
+    require("dbconnect.php");
+
+  
+
+?>
+
+
 <html>
-    
+<body style="background-color:#F379F2;">
+        </body>
 </html>
+
+
+<?php
+      $query="select * from users";
+      $result=mysqli_query($conn,$query);
+      
+        echo"<center><fieldset><legend>User Detail</legend><table border='1'>";
+        echo"<tr><td>id</td><td>UserName</td><td>Gmail</td><td>Mobile</td><td>Password</td><td colspan=2>Action</td></tr>";
+        while ($r=mysqli_fetch_array($result)) {
+        echo "<tr><td>".$r["id"]."</td><td>".$r["username"]."</td><td>".$r["email"]."</td><td>".$r["mobile"]."</td><td>".$r["password"]."</td><td><a href='editmodule.php'>edit</a></td></tr>";
+        
+      }
+      echo"</table></fieldset></center>";
+
+?>
